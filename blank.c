ce8a637287c0812c66d5e852c13424fee029f415
Sharon.Buch <buch.sharon@googlemail.com>
2022-12-08 09:38:08 +0100

    initial commit

---
+++ blank.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main () {
+
+	/*
+	*
+	*/
+
+	printf("\n *************************************** ");
+	printf("\n *   ^,,,,^   *                        * ");
+	printf("\n *  ( o x o)  *                        * ");
+	printf("\n *  ( 7   7)  *                        * ");
+	printf("\n *   ´´´´´´   *                ©S.Buch * ");
+	printf("\n *************************************** \n\n");
+	
+	//
+
+	//Ende
+	printf("\n\n");
+	return 0;
+}
